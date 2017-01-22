81affbeb880bfb6244b459015e497e8c6c80cc48
Khalil Saboor <themaninthefoxhole@gmail.com>
2017-01-22 13:39:03 -0500

    Numbers limits

--- Numbers.playground/Contents.swift
+++ Numbers.playground/Contents.swift
@@ -4,5 +4,20 @@ import UIKit
 
 var str = "Hello, playground"
 
+    print(str)
+
     print("The maximun Int value is \(Int.max).")
     print("The minimun Int value is \(Int.min)")
+    print("The maximun value for a 32-bit integer is \(Int32.max)")
+    print("The minimun value for a 32-bit integer is \(Int32.min)")
+
+    print("The maximun UInt value is \(UInt.max)")
+    print("The minimun UInt value is \(UInt.min)")
+    print("The maximun value for a 32-bit unsigned integar is \(UInt32.max)")
+    print("The minimun value for a 32-bit unsigned integar is \(UInt32.min)")
+
+    print(45 + 67)
+    print(99/2)
+    print(99%2)
+
+    
