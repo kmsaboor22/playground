45d9b86972d20ab2a58970bee8379ed7429249c9
Khalil Saboor <themaninthefoxhole@gmail.com>
2017-06-13 20:07:23 -0400

    app

---
+++ FIRST-IOS-APP/FIRST-IOS-APP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FIRST-IOS-APP
+//
+//  Created by Khalil Saboor on 6/9/17.
+//  Copyright © 2017 Khalil Saboor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
